b90ac9bd6feb4edd18ad795b526c7d066e25bafe
raulnabarret <raulnabarret@gmail.com>
2016-09-14 20:39:31 -0500

    Add Chart.js

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+var ctx=document.getElementById("myChart"),myChart=new Chart(ctx,{type:"bar",data:{labels:["Red","Blue","Yellow","Green","Purple","Orange"],datasets:[{label:"# of Votes",data:[12,19,3,5,2,3],backgroundColor:["rgba(54, 162, 235, 0.2)"],borderColor:["rgba(54, 162, 235, 1)"],borderWidth:1}]},options:{scales:{yAxes:[{ticks:{beginAtZero:!0}}]}}});
