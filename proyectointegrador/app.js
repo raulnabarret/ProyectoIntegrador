ed505404c8df806d79d4db014aa67044fbae0019
raulnabarret <raulnabarret@gmail.com>
2016-09-17 14:21:18 -0500

    Test using mongoose

--- proyectointegrador/app.js
+++ proyectointegrador/app.js
@@ -1,3 +1,4 @@
+require('./db')
 var express = require('express')
 var path = require('path')
 var favicon = require('serve-favicon')
@@ -7,6 +8,7 @@ var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 
 var routes = require('./routes/index')
+var form = require('./routes/form')
 // var users = require('./routes/users')
 
 var app = express()
@@ -24,6 +26,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', routes)
+app.use('/form', form)
+app.use('/create', form)
 // app.use('/users', users)
 
 // catch 404 and forward to error handler
